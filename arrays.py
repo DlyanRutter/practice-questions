812e277ebc3c3bcce87f3b6e58855e8c6c827949
Dylan Rutter <dylan.rutter@my.wheaton.edu>
2017-09-12 15:51:53 -0500

    Update arrays.py
    Added a function for binary search

--- arrays.py
+++ arrays.py
@@ -1,1 +1,20 @@
-pass
+def binary_search(key, array):
+    """
+    implements binary search algorithm. returns the key of a given value if it
+    is in an array. if the key is not in the array, returns None
+    """
+    low = 0
+    high = len(array) - 1
+
+    while low <= high:
+        mid = low + (high - low)/2
+
+        if array[mid] == key:
+            return mid
+
+        if key < array[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+            
+    return False
